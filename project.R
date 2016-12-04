9b608e4a4c45bc91032b011c7ba453efaee71383
Will Bradbeer <willbradbeer@gmail.com>
2016-12-04 10:49:43 -0500

    Residual Plots.

--- project.R
+++ project.R
@@ -4,8 +4,8 @@ strike <- read.csv("strikes_clean.csv")
 # encode categoricals as Factors
 strike$Centr <- as.factor(strike$Centr)
 strike$Country <- as.factor(strike$Country)
-strike$Unemp2 <- strike$Unemp^2
-strike$Demo2 <- strike$Demo^2
+#strike$Unemp2 <- strike$Unemp^2
+#strike$Demo2 <- strike$Demo^2
 strike$Infl2 <- strike$Infl^2
 
 # Investigate to look for patterns  
@@ -20,12 +20,17 @@ anova(M1)
 # remove Unemp from M1 to get current model that works well 
 M2 = lm(Strike ~ Infl*Centr, data = strike)
 
-M3 = lm(Strike ~ Infl*Centr, data = strike)
+M3 = lm(Strike ~ Infl2*Centr, data = strike)
+
+M4 = lm(Strike ~ Country*Year, data = strike)
+
+M5 = lm(Strike ~ Infl*Centr + Country, data = strike)
+
 
 # basic model
 M0 <- lm(Strike ~ 1, data = strike)
 # full model without country - interaction effects
-Mfull <- lm(Strike ~ (. - Country - Year)^2 , data = strike)
+Mfull <- lm(Strike ~ (. - Country - Year)^2 + Country, data = strike)
 # forward step model from intercept to full
 Mfwd <- step(M0, scope=list(lower=M0, upper=Mfull), direction='forward', trace=FALSE)
 # backward step model from full to intercept
@@ -34,9 +39,9 @@ Mback <- step(Mfull, scope=list(lower=M0, upper=Mfull), direction='backward', tr
 Mstep <- step(M2, scope=list(lower=M0, upper=Mfull), direction='both', trace=FALSE)
 
 
-models <- list(M0, M1, M2, M3, Mfwd, Mback, Mstep)
+models <- list(M2, )
 
-
+ 
 n <- length(strike$Strike)
 samp.size <- 400
 results <- rep(0, length(models))
@@ -60,18 +65,33 @@ for(i in 1:2000) {
 }
 
 # model diagnostics
-res <- resid(Mback)
-H <-  model.matrix(Mback)
+res <- resid(Mfwd)
+H <-  model.matrix(Mfwd)
 H <- H %*% solve(crossprod(H), t(H))
 h <- diag(H)
 # studentized residuals
 res.stu <- res/sqrt(1-h)
 
 # standardized student residuals
-res.stand <- res.stu/sigma(Mback)
+res.stand <- res.stu/sigma(Mfwd)
 
 # diagnostic plots
-plot(predict(Mback), res.stu) # seems like multiplicative error
-hist(res.stu,breaks=100)
+# scatter plot of residuals vs predictions
+cex <- 0.8 
+par(mar = c(4,4,0.1,0.1))
+plot(predict(Mfwd), res, pch=21, bg="black", cex=cex, cex.axis=cex, 
+     xlab="Predicted # of strike days", ylab="Residual Strike days")
+points(predict(Mfwd), res.stu, pch=21, bg="red", cex=cex)
+legend(x="topleft", c("Residuals", "Studentized Residuals"), pch=21,
+       pt.bg = c("black", "red"), pt.cex=cex, cex=cex)
+# seems like multiplicative error
+
+# histogram of studentized residuals
+par(mar = c(4,4,0.1,0.1))
+hist.plot <- hist(res.stand, breaks=100, freq = FALSE, cex.axis=cex, main="",
+                  xlab="Standardized, studentized residual strike activity")
+curve(dnorm(x), col='red', add=TRUE)
+
+# leverage
 
 
