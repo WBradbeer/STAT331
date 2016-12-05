7eda53782e0189db2e2c934b8641bc1b02e0427e
Isaac Hunter <isaac.hunter.c@gmail.com>
2016-12-05 15:34:23 -0500

    update appendix with Karthigas stuff

--- Appendix.R
+++ Appendix.R
@@ -129,7 +129,8 @@ h_a <- diag(H_a)
 res.stu_a <- res_a/sqrt(1-h_a)
 
 # Standardized student residuals
-res.stand_a <- res.stu_a/sigma(M_a)
+res.sta_a = res_a/sigma(M_a)
+res.stastu_a <- (res_a/sigma(M_a))/sqrt(1-h_a)
 
 cex <- 0.8 
 par(mar = c(4,4,0.1,0.1))
@@ -149,7 +150,8 @@ h_b <- diag(H_b)
 res.stu_b <- res_b/sqrt(1-h_b)
 
 # Standardized student residuals
-res.stand_b <- res.stu_b/sigma(M_b)
+res.sta_b = res_b/sigma(M_b)
+res.stastu_b <- (res_b/sigma(M_b))/sqrt(1-h_b)
 
 cex <- 0.8 
 par(mar = c(4,4,0.1,0.1))
@@ -163,12 +165,213 @@ legend(x="topleft", c("Residuals", "Studentized Residuals"), pch=21,
 
 # Next, we will plot the histogram of studentized residuals for M_a
 par(mar = c(4,4,0.1,0.1))
-hist.plot_a <- hist(res.stand_a, breaks=75, freq = FALSE, cex.axis=cex, main="",
+hist.plot_a <- hist(res.stastu_a, breaks=75, freq = FALSE, cex.axis=cex, main="",
                    xlab="Standardized, studentized residual strike activity")
 curve(dnorm(x), col='red', add=TRUE)
 
 # Next, we will plot the histogram of studentized residuals for M_b
 par(mar = c(4,4,0.1,0.1))
-hist.plot_b <- hist(res.stand_b, breaks=100, freq = FALSE, cex.axis=cex, main = "",
+hist.plot_b <- hist(res.stastu_b, breaks=100, freq = FALSE, cex.axis=cex, main = "",
                    xlab="Standardized, studentized residual strike activity")
 curve(dnorm(x), col='red', add=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Section 3.2: Leverage and Influence Measures
+
+
+#Leverage and Influence Measures of M_a
+
+head(diag(H_a)) 
+hat_a <- hatvalues(M_a) # the R way
+range(hat_a - diag(H_a)) 
+y.hat_a <- predict(M_a)
+sigma.hat_a <- summary(M_a)$sigma
+
+# PRESS residuals
+press_a <- res_a/(1-hat_a)
+# DFFITS residuals
+dfts_a <- dffits(M_a) # the R way 
+
+# standardize each of these suchthat they are identical at the average leverage value
+p_a <- length(coef(M_a))
+n_a <- nobs(M_a)
+hbar_a <- p_a/n_a # average leverage
+res.stastu_a <- res.stastu_a*sqrt(1-hbar_a) # at hat_a = hbar_a, res.stastu_a = res.sta_a
+press_a <- press_a*(1-hbar_a)/sigma.hat_a # at hat_a = hbar_a, press_a = res.sta_a
+dfts_a <- dfts_a*(1-hbar_a)/sqrt(hbar_a)# at hat_a = hbar_a, dfts_a = res.sta_a 
+
+clrs <- rep("black", len = n_a)
+strike_a = data.frame(strike$Strike, strike$Centr, strike$Infl)
+pairs(strike_a, pch = 21, bg = clrs)
+
+
+
+# plot all residuals
+par(mfrow = c(1,2), mar = c(4,4,.1,.1)) # against predicted values
+cex <- .8
+plot(y.hat_a, rep(0, length(y.hat_a)), type = "n", # empty plot to get the axis range
+     ylim = range(res.sta_a, res.stastu_a, press_a, dfts_a), cex.axis = cex,
+     xlab = "Predicted Values", ylab = "Residuals")
+# dotted line connecting each observations residuals for better visibility 
+segments(x0 = y.hat_a,
+         y0 = pmin(res.sta_a, res.stastu_a, press_a, dfts_a),
+         y1 = pmax(res.sta_a, res.stastu_a, press_a, dfts_a),
+         lty = 2)
+points(y.hat_a, res.sta_a, pch = 21, bg = "black", cex = cex)
+points(y.hat_a, res.stastu_a, pch = 21, bg = "blue", cex = cex)
+points(y.hat_a, press_a, pch = 21, bg = "red", cex = cex)
+points(y.hat_a, dfts_a, pch = 21, bg= "orange", cex = cex)
+# against leverages
+plot(hat_a, rep(0, length(y.hat_a)), type = "n", cex.axis = cex,
+     ylim = range(res.sta_a, res.stastu_a, press_a, dfts_a),
+     xlab = "Leverages", ylab = "Residuals")
+segments(x0 = hat_a,
+         y0 = pmin(res.sta_a, res.stastu_a, press_a, dfts_a),
+         y1 = pmax(res.sta_a, res.stastu_a, press_a, dfts_a),
+         lty = 2)
+points(hat_a, res.sta_a, pch = 21, bg= "black", cex = cex)
+points(hat_a, res.stastu_a, pch = 21, bg= "blue", cex = cex)
+points(hat_a, press_a, pch = 21, bg = "red", cex = cex)
+points(hat_a, dfts_a, pch = 21, bg = "orange", cex = cex)
+abline(v = hbar_a, col = "grey60",lty = 2)
+legend("topright", legend = c("Standardized", "Studentized", "PRESS", "DFFITS"),
+       pch = 21, pt.bg = c("black", "blue", "red", "orange"), title = "Residual Type:",
+       cex = cex, pt.cex = cex,)
+
+# cook's distance vs. leverage
+D_a <- cooks.distance(M_a)
+# flag some of the points
+infl.ind <- which.max(D_a) # top influence point
+
+lev.ind <- hat_a > 2*hbar_a # leveragemore than 2x the average
+clrs[lev.ind] <- "blue"
+clrs[infl.ind] <- "red"
+par(mfrow = c(1,1), mar = c(4,4,1,1))
+cex <- .8
+plot(hat_a, D_a, xlab = "Leverage", ylab = "Cook's Influence Measure",
+     pch = 21, bg = clrs, cex = cex, cex.axis = cex)
+p_a <- length(coef(M_a))
+n_a <- nrow(strike)
+hbar_a <- p_a/n_a # average leverage
+abline(v = 2*hbar_a, col = "grey60", lty = 2) # 2x average leverage
+legend("topleft", legend = c("High Leverage", "High Influence"), pch = 21,
+       pt.bg = c("blue", "red"),cex = cex, pt.cex = cex)
+
+# pairs plot of all covariates to see high leverage & influence
+pairs(strike_a, pch = 21, bg = clrs)
+
+# predictions with omitted values
+omit.ind <- c(infl.ind, # most influential
+              which.max(hat_a)) # highest leverage
+names(omit.ind) <- c("infl", "lev")
+omit.ind
+
+
+
+
+#Leverage and Influence Measures of M_b
+
+head(diag(H_b)) 
+hat_b <- hatvalues(M_b) # the R way
+range(hat_b - diag(H_b)) 
+y.hat_b <- predict(M_b)
+sigma.hat_b <- summary(M_b)$sigma
+
+# PRESS residuals
+press_b <- res_b/(1-hat_b)
+# DFFITS residuals
+dfts_b <- dffits(M_b) # the R way 
+
+# standardize each of these suchthat they are identical at the average leverage value
+p_b <- length(coef(M_b))
+n_b <- nobs(M_b)
+hbar_b <- p_b/n_b # average leverage
+res.stastu_b <- res.stastu_b*sqrt(1-hbar_b) # at hat_b = hbar_b, res.stastu_b = res.sta_b
+press_b <- press_b*(1-hbar_b)/sigma.hat_b # at hat_b = hbar_b, press_b = res.sta_b
+dfts_b <- dfts_b*(1-hbar_b)/sqrt(hbar_b)# at hat_b = hbar_b, dfts_b = res.sta_b 
+
+clrs <- rep("black", len = n_b)
+strike_b = data.frame(strike$Strike, strike$Country, strike$Infl, strike$Unemp)
+pairs(strike_b, pch = 21, bg = clrs)
+
+
+
+# plot all residuals
+par(mfrow = c(1,2), mar = c(4,4,.1,.1)) # against predicted values
+cex <- .8
+plot(y.hat_b, rep(0, length(y.hat_b)), type = "n", # empty plot to get the axis range
+     ylim = range(res.sta_b, res.stastu_b, press_b, dfts_b), cex.axis = cex,
+     xlab = "Predicted Values", ylab = "Residuals")
+# dotted line connecting each observations residuals for better visibility 
+segments(x0 = y.hat_b,
+         y0 = pmin(res.sta_b, res.stastu_b, press_b, dfts_b),
+         y1 = pmax(res.sta_b, res.stastu_b, press_b, dfts_b),
+         lty = 2)
+points(y.hat_b, res.sta_b, pch = 21, bg = "black", cex = cex)
+points(y.hat_b, res.stastu_b, pch = 21, bg = "blue", cex = cex)
+points(y.hat_b, press_b, pch = 21, bg = "red", cex = cex)
+points(y.hat_b, dfts_b, pch = 21, bg= "orange", cex = cex)
+# against leverages
+plot(hat_b, rep(0, length(y.hat_b)), type = "n", cex.axis = cex,
+     ylim = range(res.sta_b, res.stastu_b, press_b, dfts_b),
+     xlab = "Leverages", ylab = "Residuals")
+segments(x0 = hat_b,
+         y0 = pmin(res.sta_b, res.stastu_b, press_b, dfts_b),
+         y1 = pmax(res.sta_b, res.stastu_b, press_b, dfts_b),
+         lty = 2)
+points(hat_b, res.sta_b, pch = 21, bg= "black", cex = cex)
+points(hat_b, res.stastu_b, pch = 21, bg= "blue", cex = cex)
+points(hat_b, press_b, pch = 21, bg = "red", cex = cex)
+points(hat_b, dfts_b, pch = 21, bg = "orange", cex = cex)
+abline(v = hbar_b, col = "grey60",lty = 2)
+legend("topright", legend = c("Standardized", "Studentized", "PRESS", "DFFITS"),
+       pch = 21, pt.bg = c("black", "blue", "red", "orange"), title = "Residual Type:",
+       cex = cex, pt.cex = cex,)
+
+# cook's distance vs. leverage
+D_b <- cooks.distance(M_b)
+# flag some of the points
+infl.ind <- which.max(D_b) # top influence point
+
+lev.ind <- hat_b > 2*hbar_b # leveragemore than 2x the average
+clrs[lev.ind] <- "blue"
+clrs[infl.ind] <- "red"
+par(mfrow = c(1,1), mar = c(4,4,1,1))
+cex <- .8
+plot(hat_b, D_b, xlab = "Leverage", ylab = "Cook's Influence Measure",
+     pch = 21, bg = clrs, cex = cex, cex.axis = cex)
+p_b <- length(coef(M_b))
+n_b <- nrow(strike)
+hbar_b <- p_b/n_b # average leverage
+abline(v = 2*hbar_b, col = "grey60", lty = 2) # 2x average leverage
+legend("topleft", legend = c("High Leverage", "High Influence"), pch = 21,
+       pt.bg = c("blue", "red"),cex = cex, pt.cex = cex)
+
+# pairs plot of all covariates to see high leverage & influence
+pairs(strike_b, pch = 21, bg = clrs)
+
+# predictions with omitted values
+omit.ind <- c(infl.ind, # most influential
+              which.max(hat_b)) # highest leverage
+names(omit.ind) <- c("infl", "lev")
+omit.ind
+
+
+
+press_a <-  resid(M_a)/(1-hatvalues(M_a)) # M2
+press_b <- resid(M_b)/(1-hatvalues(M_b)) # Mfwd
+c(PRESS_a = sum(press_a^2), PRESS_b = sum(press_b^2)) # favors M2
